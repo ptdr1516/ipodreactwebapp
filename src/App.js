010af15acadbb2160e8231e91d43142b3ec24cdc
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-01-12 13:43:41 +0530

    Added Functionality to choose the menu to display and handle the Menu Clicks

--- src/App.js
+++ src/App.js
@@ -73,6 +73,56 @@ class App extends React.Component {
         this.controllerRef = React.createRef();
         this.progressRef = React.createRef();
     }
+
+    // Menu Functionality
+    isMenuVisible = (menu, screen) => {
+        const { songsList } = this.state;
+
+        if (menu.pageRender === "yes"){
+            menu.menuVisible = "yes";
+            screen.screenIndex = screen.wallpaperIndex;
+            menu.pageRender = "no";
+
+            songsList.songs.map((song) => {
+                song.pause();
+                song.currentTime = 0;
+                return [];
+            });
+            songsList.isPlaying = false;
+        }
+
+        else {
+            if (menu.menuVisible === "yes" &&
+                menu.musicVisible === "no" &&
+                menu.settingsVisible === "no"
+            )
+            {
+                menu.menuVisible = "no";
+            }
+            else if
+                (menu.menuVisible === "yes" &&
+                 menu.musicVisible === "yes" &&
+                 menu.settingsVisible === "no"
+            )
+            {
+                menu.musicVisible = "no";
+            }
+            else if
+               (menu.menuVisible === "yes" &&
+                menu.musicVisible === "no" &&
+                menu.settingsVisible === "yes"
+            )
+            {
+                menu.settingsVisible = "no";
+            }
+            else {
+                menu.menuVisible = "yes";
+            }
+        }
+        this.setState({
+            menu, screen, songsList
+        });
+    }
 }
 
 
